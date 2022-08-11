83b3e514d99c272d0a669d5f3d9f7fb462c52608
sseriivolk <74564915+sseriivolk@users.noreply.github.com>
2022-08-11 15:15:19 +0200

    Update outputs.tf
    Use the nonsensitive function in the output

--- configuration/iaac/azure/kubernetes/outputs.tf
+++ configuration/iaac/azure/kubernetes/outputs.tf
@@ -23,6 +23,6 @@ output "kube_config" {
 }
 
 output "host" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.host
-  sensitive = true
+  value = nonsensitive(azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.host)
+#  sensitive = true
 }
